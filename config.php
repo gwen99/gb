50d416fde42a7ad4bd07d802450ea04d6f0f0b42
gwen99 <45752195+gwen99@users.noreply.github.com>
2018-12-18 10:49:07 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+///Jangan hapus tanpa kutip nya(")
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+///Ganti xxxxxxx dengan punya kalian
+///MATUR NUWUN KIK NO....
+$ticket = "xxxxxxxxx";
+$meid = "xxxxxxxxxx";
+$os="xxxxxxxx";
+$sign="xxxxxxxxx";
